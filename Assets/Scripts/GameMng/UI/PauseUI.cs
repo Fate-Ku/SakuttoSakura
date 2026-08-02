5ea9c6ec791075cbe8d948f0ee8c5d135eb3d0b9
Fate-Ku <elaina42fate@gmail.com>
2026-08-03 00:35:13 +0900

    2026/8/3 update tutorial scene

--- Assets/Scripts/GameMng/UI/PauseUI.cs
+++ Assets/Scripts/GameMng/UI/PauseUI.cs
@@ -2,6 +2,8 @@
 // PauseUI.cs
 // 
 // 2026/06/14 Created By Fate Ku 
+// 2026/08/03 Created By Fate Ku 
+//
 
 using UnityEngine;
 
@@ -9,6 +11,10 @@ public class PauseUI : MonoBehaviour
 {
     [Header("Popup UI")]
     public GameObject popupPanel;
+
+    [Header("button frame")]
+    public GameObject buttonPanel;
+
 
     public void ShowPauseOptions()
     {
@@ -19,6 +25,20 @@ public class PauseUI : MonoBehaviour
     {
         popupPanel.SetActive(false);
     }
+
+
+    // 2026/08/03 Created By Fate Ku 
+    public void ShowBtnFrame()
+    {
+        buttonPanel.SetActive(true);
+    }
+
+    public void HideBtnFrame()
+    {
+        buttonPanel.SetActive(false);
+    }
+    // 2026/08/03 Created By Fate Ku 
+
 
     public void Pause()
     {
