d7ed572593a6b4a64ae818595621895c7b47d001
Fate-Ku <elaina42fate@gmail.com>
2026-09-12 20:29:35 +0900

    2026/9/12 tutorial scene

--- Assets/Scripts/GameMng/UI/Tutorial/TutorialInfo.cs
+++ Assets/Scripts/GameMng/UI/Tutorial/TutorialInfo.cs
@@ -3,6 +3,7 @@
 // 
 // 2026/08/01 Created By Fate Ku
 // 2026/09/09 Updated By Fate Ku
+// 2026/09/12 Updated By Fate Ku
 //
 
 using TMPro;
@@ -17,6 +18,20 @@ public class TutorialInfo : MonoBehaviour
     [Header("Click")]
     [SerializeField] public GameObject ClickMark;
     [SerializeField] public GameObject TapFrame;
+    [SerializeField] public GameObject InfoFrame;
+    [SerializeField] public GameObject TapWordAnim;
+
+    [Header("Pause UI")]
+    public GameObject popupPanel;
+
+    public GameObject GetInfoFrame()
+    {  return InfoFrame; }
+
+    public GameObject GetTapWordAnim()
+    { return TapWordAnim; }
+
+    public GameObject GetPopupPanel()
+    { return popupPanel; }
 
     public TextMeshProUGUI GetInstructionsText()
     {
